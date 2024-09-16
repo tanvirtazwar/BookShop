c1707009fffcba661c2e3a18ed14b082385e8228
tanvirtazwar <tanvirtazwar37@gmail.com>
2024-09-16 11:26:42 +0600

    Seeded category table.

--- BookShopWeb/Migrations/20240916051037_ModifyingCategoriesTable.Designer.cs
+++ BookShopWeb/Migrations/20240916051037_ModifyingCategoriesTable.Designer.cs
@@ -3,6 +3,7 @@ using BookShopWeb.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BookShopWeb.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240916051037_ModifyingCategoriesTable")]
+    partial class ModifyingCategoriesTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -39,26 +42,6 @@ namespace BookShopWeb.Migrations
                     b.HasKey("Id");
 
                     b.ToTable("Categories");
-
-                    b.HasData(
-                        new
-                        {
-                            Id = 1,
-                            DisplayOrder = 1,
-                            Name = "Action"
-                        },
-                        new
-                        {
-                            Id = 2,
-                            DisplayOrder = 2,
-                            Name = "SciFi"
-                        },
-                        new
-                        {
-                            Id = 3,
-                            DisplayOrder = 3,
-                            Name = "History"
-                        });
                 });
 #pragma warning restore 612, 618
         }
--- BookShopWeb/Models/Category.cs
+++ BookShopWeb/Models/Category.cs
@@ -8,6 +8,6 @@ namespace BookShopWeb.Models
         public int Id { get; set; }
         [Required] 
         public string Name { get; set; }
-        public string DisplayOrder { get; set; }
+        public int DisplayOrder { get; set; }
     }
 }
