2de4d06a6ec8d662448e63798389582cbfd2c29a
tanvirtazwar <tanvirtazwar37@gmail.com>
2024-09-17 22:55:12 +0600

    Get category details to edit

--- BookShopWeb/Controllers/CategoryController.cs
+++ BookShopWeb/Controllers/CategoryController.cs
@@ -41,5 +41,37 @@ namespace BookShopWeb.Controllers
             }
             return View();
         }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? category = db.Categories.FirstOrDefault(c => c.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            if (category.Name != null && !(category.Name.All(char.IsLetter)))
+            {
+                ModelState.AddModelError("Name", "The Category" +
+                    " Name must contain only letters.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                db.Categories.Add(category);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
     }
 }
